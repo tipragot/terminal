a95a8894d3a29bbc18cb18573f2b1d78f42bf552
tcezard <tcezard@student.42nice.fr>
2024-11-06 12:03:44 +0000

    Add man entries

--- launch.sh
+++ launch.sh
@@ -5,6 +5,7 @@ docker run --rm \
 	--privileged -e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri \
 	-v /run/user/$(id -u $USER)/docker.sock:/var/run/docker.sock \
+	-v /usr/share/man:/usr/share/man \
 	-e LOGIN=$USER \
 	-v "$HOME/.terminal/ssh:/home/me/.ssh" \
 	-v "$HOME/.terminal/cache:/home/me/.cache" \
