28234e831ab033232c2b51cd4e41168f40ddc47e
tcezard <tcezard@student.42nice.fr>
2024-11-06 12:01:10 +0100

    Pass the docker socket to the container

--- launch.sh
+++ launch.sh
@@ -4,6 +4,7 @@ xhost +local:root
 docker run --rm \
 	-e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri:/dev/dri \
+	-v /run/user/$(id -u $USER)/docker.sock:/var/run/docker.sock \
 	-e LOGIN=$USER \
 	-v "$HOME/.terminal/ssh:/home/me/.ssh" \
 	-v "$HOME/.terminal/cache:/home/me/.cache" \
